3bbaf05fc9683568c4f4fdee527a043f0d5f6b53
mnikn <329121948@qq.com>
2018-10-11 18:43:58 +0800

    [feature]add role list

---
+++ src/app/dnd/dnd3r/factory/role-builder.ts
@@ -0,0 +1,33 @@
+import { Role } from '../models/role';
+import { Profession } from '../models/profession';
+import * as _ from 'lodash';
+
+export class RoleBuilder {
+
+  private _role: Role;
+
+  constructor() {
+    this._role = new Role();
+  }
+
+  public setId(id: number): RoleBuilder {
+    this._role.id = id;
+    return this;
+  }
+
+  public setBasicsInfo(name: string, age: number, description?: string): RoleBuilder {
+    this._role.name = name;
+    this._role.age = age;
+    this._role.description = description;
+    return this;
+  }
+
+  public setProfession(profession: Profession): RoleBuilder {
+    this._role.profession = profession;
+    return this;
+  }
+
+  public build(): Role {
+    return _.cloneDeep(this._role);
+  }
+}
